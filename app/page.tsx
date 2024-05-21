929f937b375fbecad88137c1bc8054383d562c5d
nathandl1 <104229699+nathandl1@users.noreply.github.com>
2024-05-21 11:32:34 -0700

    added nextUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Button } from "@nextui-org/button";
 
 export default function Home() {
   return (
     <main>
-      
+      <Button>Click Me!</Button>
     </main>
   );
 }
