5b2ac2bf2f2910154f8fac9e076115c5993d20aa
nathandl1 <104229699+nathandl1@users.noreply.github.com>
2024-05-23 12:35:49 -0700

    huge calendar updates

--- app/dashboard/calendar/calendar.tsx
+++ app/dashboard/calendar/calendar.tsx
@@ -3,81 +3,55 @@ import React from 'react'
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Image} from "@nextui-org/react";
 
 
-export default function Calendar() {
+export default function Calendar({birthday, event, season}) {
   return (
     <div>
-        
-        <Table hideHeader aria-label="Example empty table" className='text-xl'>
-            <TableHeader>
-                <TableColumn>Sunday</TableColumn>
-                <TableColumn>Monday</TableColumn>
-                <TableColumn>Tuesday</TableColumn>
-                <TableColumn>Wednesday</TableColumn>
-                <TableColumn>Thursday</TableColumn>
-                <TableColumn>Friday</TableColumn>
-                <TableColumn>Saturday</TableColumn>
-            </TableHeader>
-            <TableBody>
-                <TableRow key="1">
-                <TableCell>
-                    1
-                    <Image 
-                        width='100%'
-                        src='/villagers/Penny.png'
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                <TableCell>
-                    2
-                    <Image 
-                        width='100%'
-                        src='/villagers/Harvey.png'
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                <TableCell>
-                    3
-                    <Image 
-                        width='100%'
-                        src='/villagers/Jodi.png'
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                <TableCell>
-                    4
-                    <Image 
-                        width='100%'
-                        src='/villagers/Shane.png'
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                <TableCell>
-                    5
-                    <Image 
-                        width='100%'
-                        src='/villagers/Lewis.png'
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                <TableCell>
-                    6
-                    <Image 
-                        width='100%'
-                        src='/villagers/Willy.png'
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                <TableCell>
-                    7
-                    <Image 
-                        width='100%'
-                        src={'/villagers/'+'Sam'+'.png'}
-                        alt='Penny from Stardew Valley'
-                    />
-                </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+        <div className="grid grid-cols-7 gap-4">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+            <div>04
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Kent.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>05</div>
+            <div>06</div>
+            <div>07
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Lewis.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>08</div>
+            <div>09</div>
+            <div>10
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Vincent.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>11</div>
+            <div>12</div>
+            <div>13</div>
+            <div>14
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Haley.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>15</div>
+            <div>16</div>
+            <div>17</div>
+            <div>18
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Pam.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>19</div>
+            <div>20
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Shane.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>21</div>
+            <div>22</div>
+            <div>23</div>
+            <div>24</div>
+            <div>25</div>
+            <div>26
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Pierre.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>27
+                {birthday && season=='Spring' ? <Image width='100%' src='/villagers/Emily.png' alt='Penny from Stardew Valley'/> : <></>}
+            </div>
+            <div>28</div>
+        </div>
     </div>
   );
 }
--- app/dashboard/calendar/page.tsx
+++ app/dashboard/calendar/page.tsx
@@ -1,11 +1,53 @@
+'use client'
 import React from 'react'
 import Calendar from './calendar'
+import { useState } from 'react'
+import { Button } from '@nextui-org/react';
+import {  Dropdown,  DropdownTrigger,  DropdownMenu,  DropdownSection,  DropdownItem} from "@nextui-org/dropdown";
+
 
 export default function CalendarPage() {
+  const [birthday, setBirthday] = useState(false);
+  const [event, setEvent] = useState(false);
+  const [season, setSeason] = useState('Spring');
+  
+
+  const toggleBday = () =>{
+    setBirthday(!birthday);
+  }
+
+  const toggleEvent = () =>{
+    setEvent(!event);
+  }
+
+  const changeSeason = (newSeason) =>{
+    setSeason(newSeason);
+  }
+
   return (
     <div>
       Calendar Page
-      <Calendar/>
+      <div>
+        <Button onClick={toggleBday}>BirfDay?</Button>
+        <Button onClick={toggleEvent}>Ebent?</Button>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button 
+              variant="bordered" 
+            >
+              {season}
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions" onAction={(key) => changeSeason(key)}>
+            <DropdownItem key="Spring">Spring</DropdownItem>
+            <DropdownItem key="Summer">Summer</DropdownItem>
+            <DropdownItem key="Fall">Fall</DropdownItem>
+            <DropdownItem key="Winter">Winter</DropdownItem>
+          </DropdownMenu>
+      </Dropdown>
+      </div>
+      
+      <Calendar  birthday={birthday} event={false} season={season}/>
     </div>
   )
 }
