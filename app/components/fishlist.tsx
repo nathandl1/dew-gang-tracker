6059495b9360b077a6e6107eac5e409ca08b801d
nathandl1 <104229699+nathandl1@users.noreply.github.com>
2024-06-06 16:49:20 -0700

    worked on fishmap page

---
+++ app/components/fishlist.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+export default function Fishlist({day, weather}) {
+  return (
+    <div>
+        <p>Day is: {day} Weather is: {weather}</p>
+    </div>
+  )
+}
--- app/dashboard/fishmap/page.tsx
+++ app/dashboard/fishmap/page.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+'use client'
+import { Image, Radio, RadioGroup, Slider } from '@nextui-org/react'
+import React, { useState } from 'react'
+import Fishlist from '@/app/components/fishlist';
 
 export default function FishMapPage() {
+  const [value, setValue] = useState(1);
+  const [selected, setSelected] = useState("Sun");
   return (
-    <img src = "/Images/Map.png" className = "w-5/6 m-auto"></img>
+    <div className='bg-white'>
+      <Slider   
+        size="lg"
+        step={1}
+        color="foreground"
+        label="Day of the Season"
+        showSteps={true} 
+        maxValue={28} 
+        minValue={1} 
+        defaultValue={1}
+        className="max-w-md" 
+        value={value}
+        onChange={setValue}
+      />
+      <RadioGroup
+        label="Select Weather: "
+        value={selected}
+        onValueChange={setSelected}
+        orientation='horizontal'
+        color='success'
+      >
+        <Radio value="Sun">Sunny</Radio>
+        <Radio value="Rain">Rainy</Radio>
+        <Radio value="Wind">Windy</Radio>
+        <Radio value="Storm">Stormy</Radio>
+        <Radio value="Snow">Snowy</Radio>
+      </RadioGroup>
+      <Fishlist day={value} weather={selected} />
+    </div>
   )
 }
+
+
+// export default function App() {
+//   const [value, setValue] = React.useState(25);
+
+//   return (
+//     <div className="flex flex-col gap-2 w-full h-full max-w-md items-start justify-center">
+//       <Slider
+//         aria-label="Volume"
+//         size="lg"
+//         color="secondary"
+//         onChangeEnd={setValue}
+//         className="max-w-md"
+//       />
+//       <p className="text-default-500 font-medium text-small">Current volume: {value}</p>
+//     </div>
+//   );
+// }
