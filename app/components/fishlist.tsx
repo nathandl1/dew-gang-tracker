ad927c4944d996b187187b12d37ab38a8f4cb521
nathandl1 <104229699+nathandl1@users.noreply.github.com>
2024-06-07 01:41:02 -0700

    lots of updates to fishlist

--- app/components/fishlist.tsx
+++ app/components/fishlist.tsx
@@ -1,14 +1,52 @@
 'use client'
 import React from 'react'
-interface FishProps {
-  day: number;
-  weather:string;
-}
+import fish from '@/app/dashboard/fishfinder/fish'
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Image} from "@nextui-org/react";
 
-export default function Fishlist({day, weather}:FishProps) {
+interface FishProps {
+  season: string;
+  weather:string;
+  time:number;
+}
+const columns = [
+  {
+    key:"image",
+    label: "Image",
+  }, 
+  {
+    key:"name",
+    label: "Name",
+  }, 
+  {
+    key:"location",
+    label: "Location(s)",
+  }, 
+]
+
+export default function Fishlist({season, weather, time}:FishProps) {
+  const timeArray:string[] = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12pm', '1am', '2am'];
+  function searched(value:any){
+    return (time >= value.start && time <= value.end) && (value.season == "All" || season == value.season);
+  }
+  const filtered = fish.filter(searched);
   return (
     <div>
-        <p>Day is: {day} Weather is: {weather}</p>
+      <Table aria-label="Example table with dynamic content">
+        <TableHeader columns={columns}>
+          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+        </TableHeader>
+        <TableBody>
+          {
+            filtered.map((filter)=>(
+              <TableRow key={filter.name}>
+                <TableCell><Image src={filter.src} alt={''} width={48} radius='none'></Image></TableCell>
+                <TableCell>{filter.name}</TableCell>
+                <TableCell>{filter.location}</TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
     </div>
   )
 }
