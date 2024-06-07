e572bb7b99635cd81c3a814883cc2416f68c0377
nathandl1 <104229699+nathandl1@users.noreply.github.com>
2024-06-06 18:06:17 -0700

    Update fishlist.tsx

--- app/components/fishlist.tsx
+++ app/components/fishlist.tsx
@@ -1,7 +1,11 @@
 'use client'
 import React from 'react'
+interface FishProps {
+  day: number;
+  weather:string;
+}
 
-export default function Fishlist({day, weather}) {
+export default function Fishlist({day, weather}:FishProps) {
   return (
     <div>
         <p>Day is: {day} Weather is: {weather}</p>
